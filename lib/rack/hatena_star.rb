466332c837a18a62a77669970bf67dc343775b80
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2015-10-19 13:41:32 +0900

    bundle gem rack-hatena_star

---
+++ lib/rack/hatena_star.rb
@@ -0,0 +1,7 @@
+require "rack/hatena_star/version"
+
+module Rack
+  module HatenaStar
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/hatena_star/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module HatenaStar
+    VERSION = "0.1.0"
+  end
+end
